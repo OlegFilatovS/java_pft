d94d089d7131f6c96f3dde91e070c8edf5ced45d
OlegFilatov <joincs@yandex.ru>
2016-08-01 20:58:57 +0300

    4th Homework

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String name;
+    private final String middleName;
+    private final String lastName;
+    private final String company;
+    private final String adrress;
+
+    public ContactData(String name, String middleName, String lastName, String company, String adrress) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.company = company;
+        this.adrress = adrress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAdrress() {
+        return adrress;
+    }
+}
